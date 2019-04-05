da844c00c419970855e3d309406b878156de4478
Unknown <s110619046@stu.ntue.edu.tw>
2019-04-05 19:14:52 +0800

    5 commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,25 @@
-import 'jquery';
-
+import $ from 'jquery';
+import '../js/script';
 import bird from '../images/bird.jpg';
 import squirrel from '../images/squirrel.jpg';
+import '../css/normalize.css';
+import '../css/style.css';
+import "../fonts/PT-Sans.eot";
+import "../fonts/PT-Sans-Bold.eot";
+import "../fonts/PT-Sans-Italic.eot";
+import "../fonts/PT-Sans-BoldItalic.eot";
+import "../fonts/PT-Sans.woff";
+import "../fonts/PT-Sans-Bold.woff";
+import "../fonts/PT-Sans-Italic.woff";
+import "../fonts/PT-Sans-BoldItalic.woff";
+import "../fonts/PT-Sans.ttf";
+import "../fonts/PT-Sans-Bold.ttf";
+import "../fonts/PT-Sans-Italic.ttf";
+import "../fonts/PT-Sans-BoldItalic.ttf";
+import "../fonts/PT-Sans.svg";
+import "../fonts/PT-Sans-Bold.svg";
+import "../fonts/PT-Sans-Italic.svg";
+import "../fonts/PT-Sans-BoldItalic.svg";
 
-$(document).ready(function () {
-	// when a hanburger menu is clicked
-	let $ul = $(".nav__list"),
-		 $menu = $(".nav__menu");
-
-	$($menu).click(function () {
-		// toggle menu-click Class
-		$ul.toggleClass("menu-click");
-	}); // end click event handler
-});
-
-$('#images_bird').attr("src", bird);
-$('#images_squirrel').attr("src", squirrel);
+$('#image__bird').attr("src", bird);
+$('#image__squirrel').attr("src", squirrel);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,49 @@
-let path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
    entry: './assets/js/app.js',
    output: {
       path: path.join(__dirname, './dist'),
-      filename: 'bundle.js',
-      publicPath: './dist/'
+      filename: 'bundle.[chunkhash].js'
    },
    module: {
-      rules: [
+      rules: [{
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [{
+                  loader: 'url-loader',
+                  options: {
+                     limit: 40000,
+                     outputPath: './images'
+                  }
+               },
+               'image-webpack-loader'
+            ]
+         },
+         {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+               use: [{
+                  loader: 'css-loader',
+                  options: {
+                     url: false
+                  }
+               }, {
+                  loader: 'postcss-loader'
+               }]
+            })
+         },
+         {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            use: {
+               loader: 'file-loader',
+               options: {
+                  outputPath: 'css/fonts',
+                  name: '[name].[ext]',
+               },
+            }
+         },
          {
             test: /\.(js)$/,
             exclude: /(node_modules)/,
@@ -18,20 +53,16 @@ module.exports = {
                   presets: ['@babel/preset-env']
                }
             }
-         },
-         {
-            test: /\.(jpe?g|png|gif|svg)$/,
-            use: [
-               {
-                  loader: 'url-loader',
-                  options: {
-                     limit: 40000,
-                     outputPath: './images'
-                  }
-               },
-               'image-webpack-loader'
-            ]
          }
       ]
+   },
+   plugins: [
+      new ExtractTextPlugin('css/style.css'),
+      new HtmlWebpackPlugin({
+         template: '../index.html',
+      })
+   ],
+   devServer: {
+      contentBase: 'dist'
    }
 }
