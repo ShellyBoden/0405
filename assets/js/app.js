cc277bcf632444b5840f5dcd8985a73aa151d476
Unknown <s110619046@stu.ntue.edu.tw>
2019-04-05 17:51:35 +0800

    4 commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,18 @@
-import '../assets/js/jquery.js';
-import '../assets/js/script.js';
+import 'jquery';
+
+import bird from '../images/bird.jpg';
+import squirrel from '../images/squirrel.jpg';
+
+$(document).ready(function () {
+	// when a hanburger menu is clicked
+	let $ul = $(".nav__list"),
+		 $menu = $(".nav__menu");
+
+	$($menu).click(function () {
+		// toggle menu-click Class
+		$ul.toggleClass("menu-click");
+	}); // end click event handler
+});
+
+$('#images_bird').attr("src", bird);
+$('#images_squirrel').attr("src", squirrel);
--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,30 @@ module.exports = {
       publicPath: './dist/'
    },
    module: {
-      rules: [{
-         test: /\.(js)$/,
-         exclude: /(node_modules)/,
-         use: {
-            loader: 'babel-loader',
-            options: {
-               presets: ['@babel/preset-env']
+      rules: [
+         {
+            test: /\.(js)$/,
+            exclude: /(node_modules)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env']
+               }
             }
+         },
+         {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [
+               {
+                  loader: 'url-loader',
+                  options: {
+                     limit: 40000,
+                     outputPath: './images'
+                  }
+               },
+               'image-webpack-loader'
+            ]
          }
-      }]
+      ]
    }
 }
